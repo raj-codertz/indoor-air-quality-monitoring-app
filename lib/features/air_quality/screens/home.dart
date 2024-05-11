bf572153a9405e333f1cce4e9221ae30574015b1
rajabu shabani <rajabujshabani@gmail.com>
2024-05-11 15:54:37 +0300

    Done with primary header container

--- lib/features/air_quality/screens/home.dart
+++ lib/features/air_quality/screens/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:indoor_air_quality/utils/constants/colors.dart';
-import '../../../common/widgets/custom_shapes/containers/circular_container.dart';
-import '../../../common/widgets/custom_shapes/curved_edges/curved_edge_widget.dart';
+import '../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,35 +9,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            TCurvedEdgeWidget(
-              child: Container(
-                color: TColors.primary,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: -150,
-                        right: -250,
-                        child: TCircularContainer(
-                            backgroundColor:
-                                TColors.textWhite.withOpacity(0.1)),
-                      ),
-                      Positioned(
-                        top: 100,
-                        right: -300,
-                        child: TCircularContainer(
-                            backgroundColor:
-                                TColors.textWhite.withOpacity(0.1)),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+          children: [TPrimaryHeaderContainer(child: Container())],
         ),
       ),
     );
